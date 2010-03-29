d7d1a6463747b40210358fff82b518c2ad6ddbc4
Tucker Hermans <thermans@cc.gatech.edu>
2010-03-29 18:28:57 -0400

    More command line arguments for generate codebook.

--- generate_codebook.cpp
+++ generate_codebook.cpp
@@ -7,13 +7,42 @@
 #include "visual_codebook.h"
 using namespace std;
 
+#define MIN_CODEBOOK_ARGS 4
+#define MAX_CODEBOOK_ARGS 5
+
 int main(int argc, char** argv)
 {
+  if (argc < MIN_CODEBOOK_ARGS || argc > MAX_CODEBOOK_ARGS)
+  {
+    cerr << "usage: " << argv[0]
+         << " path/to/images/ num_images num_centers [path/to/codebook]"
+         << endl;
+    return -1;
+  }
+
+  // Get command line arguments
+  string img_path(argv[1]);
+  int num_images = atoi(argv[2]);
+  int num_centers = atoi(argv[3]);
+  string cb_path;
+
+  if(argc == MAX_CODEBOOK_ARGS)
+  {
+    cb_path = string(argv[4]);
+  }
+  else
+  {
+    cb_path = img_path;
+    cb_path.append("default.codebook");
+  }
+
   // Cluster the codewords
-  VisualCodebook vc(1000);
-  vc.constructCodebook("~/src/fab-data/city-centre/Images/", 2474);
-  
+  VisualCodebook vc(num_centers);
+  vc.constructCodebook(img_path, num_images);
+
   // Write cluster centers to file
+
+  vc.saveCodebook(cb_path);
 
   return 0;
 }
